5f613210aa1030a7785f92df910c5769797e4729
Olof Kindgren <olof.kindgren@gmail.com>
2020-03-16 10:50:05 +0100

    Add Nexys A7 support

---
+++ data/nexys_a7.tcl
@@ -0,0 +1,8 @@
+#Waive warnings
+set_msg_config -id "Synth 8-2507" -string "priority_encoder" -suppress
+set_msg_config -id "Synth 8-6014" -string "axis_arb_mux tkeep_reg" -suppress
+set_msg_config -id "Synth 8-6014" -string "axis_arb_mux tid_reg" -suppress
+set_msg_config -id "Synth 8-6014" -string "axis_arb_mux tdest_reg" -suppress
+set_msg_config -id "Synth 8-6014" -string "axis_arb_mux tuser_reg" -suppress
+set_msg_config -id "Synth 8-3331" -string "axis_arb_mux s_axis_tid" -suppress
+set_msg_config -id "Synth 8-3331" -string "axis_arb_mux s_axis_tkeep" -suppress
