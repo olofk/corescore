9899cd21663632f164bbfb68a561a6abfa4b8d8a
Alexander Binder <binderalexander@outlook.com>
2021-09-29 13:39:54 +0200

    added support for TrenzElectronics MAX1000

---
+++ data/max1000.tcl
@@ -0,0 +1,7 @@
+set_location_assignment PIN_H6 -to CLK12M
+set_location_assignment PIN_A8 -to q
+set_location_assignment PIN_A4 -to i_uart_rx
+set_location_assignment PIN_B4 -to o_uart_tx
+
+set_global_assignment -name INTERNAL_FLASH_UPDATE_MODE "SINGLE COMP IMAGE WITH ERAM"
+
