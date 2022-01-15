6a6b353c087cf61148bf79985f8ce34d8c749c06
Oguz Meteer <info@guztech.nl>
2022-01-15 01:29:15 +0100

    Add Nexys Video target
    Signed-off-by: Oguz Meteer <info@guztech.nl>

---
+++ rtl/corescore_nexys_video.v
@@ -0,0 +1,39 @@
+`default_nettype none
+module corescore_nexys_video
+(
+ input wire  i_clk,
+ output wire o_uart_tx);
+
+   wire      clk;
+   wire      rst;
+
+   nexys_video_clock_gen clock_gen
+     (.i_clk (i_clk),
+      .o_clk (clk),
+      .o_rst (rst));
+
+   parameter memfile_emitter = "emitter.hex";
+
+   wire [7:0]  tdata;
+   wire        tlast;
+   wire        tvalid;
+   wire        tready;
+
+   corescorecore corescorecore
+     (.i_clk     (clk),
+      .i_rst     (rst),
+      .o_tdata   (tdata),
+      .o_tlast   (tlast),
+      .o_tvalid  (tvalid),
+      .i_tready  (tready));
+
+   emitter #(.memfile (memfile_emitter)) emitter
+     (.i_clk     (clk),
+      .i_rst     (rst),
+      .i_tdata   (tdata),
+      .i_tlast   (tlast),
+      .i_tvalid  (tvalid),
+      .o_tready  (tready),
+      .o_uart_tx (o_uart_tx));
+
+endmodule
